3af46a93cc837e91cd2c2fbdada7103b47550b9a
Brenden-Yiping-Wang <brendenwong@outlook.com>
2022-11-04 15:17:17 -0400

    Complete the recipe(entity)

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,25 @@
+package Entities;
+import java.util.List;
+public class User {
+
+    private String username;
+    private String password;
+    private boolean LoginStatus;
+    private Profile profile;
+    private List<User> UserList;
+    private List<User> followers;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean isLoginStatus() {
+        return LoginStatus;
+    }
+
+}
