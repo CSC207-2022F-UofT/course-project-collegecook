4697234b2fde5fbd288fbd5bb78ec6a8fb6e2d00
Huayin Luo <hclover2003@gmail.com>
2022-11-04 15:48:11 -0400

    what is happening

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -4,17 +4,15 @@ public class User {
 
     private String username;
     private String password;
-    private User creator;
     private boolean LoginStatus;
     private Profile profile;
     private List<User> UserList;
     private List<User> followers;
     private List<User> followed;
 
-    public User(String username, String password, User creator) {
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
-        this.creator = creator;
     }
 
     public String getUsername() {
