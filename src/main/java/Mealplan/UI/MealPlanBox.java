b33f83d08804bc6cf9765c39446d4bfaaf944e16
luciatan <lucia.tan@mail.utoronto.ca>
2022-11-08 14:26:12 -0800

    create classes

---
+++ src/main/java/Mealplan/UI/MealPlanBox.java
@@ -0,0 +1,23 @@
+package Mealplan.UI;
+import Mealplan.controller.mealplanController;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MealPlanBox implements ActionListener{
+    JFrame mealplanbox;
+    JLabel breakfastlable, lunchlable, dinnerlable;
+    JButton breakfastbutton, lunchbutton, dinnerbutton;
+
+
+    public void createMealPlanBox(){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
