cb62870936fe6d149e149b65cbfcec4679518d1e
Brenden-Yiping-Wang <brendenwong@outlook.com>
2022-11-14 18:37:46 -0500

    update

--- src/main/java/Entities/Recipe.java
+++ src/main/java/Entities/Recipe.java
@@ -3,7 +3,7 @@ package Entities;
 import java.util.List;
 import java.io.Serializable;
 
-public class Recipe implements Serializable{
+public class Recipe implements Serializable {
     private String recipe_name;
     private String procedure;
     private String cuisine;
@@ -15,7 +15,7 @@ public class Recipe implements Serializable{
 
 
     public Recipe(String recipe_name, String procedure, String cuisine, List<String> ingredients,
-                  int calories, int time, int difficulty){
+                  int calories, int time, int difficulty) {
         this.procedure = procedure;
         this.recipe_name = recipe_name;
         this.cuisine = cuisine;
@@ -25,33 +25,52 @@ public class Recipe implements Serializable{
         this.difficulty = difficulty;
     }
 
-    public String get_procedure(){return this.procedure;}
-    public String get_recipe_name(){return this.recipe_name;
+    public String get_procedure() {
+        return this.procedure;
     }
-    public String get_cuisine(){return this.cuisine;}
-    public int get_calories(){return this.calories;}
-    public int get_time(){return this.time;}
-    public int get_difficulty(){return this.difficulty;}
 
-    public List<String> get_ingredients(){return this.ingredients;}
+    public String get_recipe_name() {
+        return this.recipe_name;
+    }
 
-    public void set_cuisine(String cuisine){this.cuisine = cuisine;}
-    public void set_calories(int calories){this.calories = calories;}
-    public void set_time(int time){this.time = time;}
-    public void set_difficulty(int difficulty){this.difficulty = difficulty;}
+    public String get_cuisine() {
+        return this.cuisine;
+    }
 
-    public void set_ingredients(List<String> ingredients){this.ingredients = ingredients;}
+    public int get_calories() {
+        return this.calories;
+    }
 
+    public int get_time() {
+        return this.time;
+    }
 
+    public int get_difficulty() {
+        return this.difficulty;
+    }
 
+    public List<String> get_ingredients() {
+        return this.ingredients;
+    }
 
+    public void set_cuisine(String cuisine) {
+        this.cuisine = cuisine;
+    }
 
+    public void set_calories(int calories) {
+        this.calories = calories;
+    }
 
+    public void set_time(int time) {
+        this.time = time;
+    }
 
+    public void set_difficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
 
-
-
-
-
+    public void set_ingredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
 
 }
--- src/main/java/Entities/RecipeList.java
+++ src/main/java/Entities/RecipeList.java
@@ -1,17 +1,40 @@
 package Entities;
 
 import java.io.Serializable;
+<<<<<<< HEAD
 import java.util.HashMap;
 import java.util.Map;
+=======
+import java.util.*;
+>>>>>>> e753dc391b5bd7aff111f4a1a626ee89c178afb9
 
 public class RecipeList implements Serializable{
     private final Map<String, Recipe> all_recipe = new HashMap<>() ;
 
+<<<<<<< HEAD
     public void add_recipe(Recipe recipe){
         this.all_recipe.put(recipe.get_recipe_name(), recipe);
     }
     public Recipe get_recipe(String recipename){
         return all_recipe.get(recipename);
+=======
+    public void add_recipe(String recipe_name, String procedure, String cuisine, List<String> ingredients,
+                           int calories, int time, int difficulty){
+        Recipe recipe = new Recipe(recipe_name, procedure, cuisine, ingredients, calories, time, difficulty);
+        this.all_recipe.put(recipe_name, recipe);
+    }
+    public Recipe get_recipe(String recipeName){
+
+        return all_recipe.get(recipeName);
+    }
+
+    public Boolean contain(String recipeName){
+        return all_recipe.containsKey(recipeName);
+    }
+
+    public String get_all_name(){
+        return all_recipe.keySet().toString();
+>>>>>>> e753dc391b5bd7aff111f4a1a626ee89c178afb9
     }
 
 }
