ade88255b44637d03bc6ebb24b4516e5bc51f538
krutikajoshi <joshikrutikassc@gmail.com>
2022-11-18 15:00:06 -0500

    adding java docs

--- src/main/java/rank/RankOutputBoundary.java
+++ src/main/java/rank/RankOutputBoundary.java
@@ -1,7 +1,19 @@
 package rank;
 
 public interface RankOutputBoundary {
+    /**
+     * A method that contains a success method that needs to be implemented
+     * for the model to output the success message with the sorted list.
+     *
+     * @param rank contains the sorted Users list
+     */
     RankResponseModel prepareSuccessView(RankResponseModel rank);
+    /**
+     * A method that contains a fail method that needs to be implemented
+     * for the model to output the failed sorting message.
+     *
+     * @param error contains the information on the error
+     */
     RankResponseModel prepareFailView(String error);
 }
 
--- src/main/java/rank/RankRequestModel.java
+++ src/main/java/rank/RankRequestModel.java
@@ -2,9 +2,21 @@ package rank;
 
 public class RankRequestModel {
     private String ranking;
+    /**
+     * Constructor for RankRequestModel. Takes in a ranking which
+     * contains the variable chosen by the user to sort the other
+     * Users.
+     *
+     * @param ranking contains variable chosen by the user to sort the other
+     *      * Users
+     */
     public RankRequestModel(String ranking) {
         this.ranking = ranking;
     }
+    /**
+     * A getter function that returns the variable chosen by
+     * the user.
+     */
     String getRanking() {
         return ranking;
     }
