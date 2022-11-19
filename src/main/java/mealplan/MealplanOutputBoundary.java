a070402198460d59f527c5f77dd03543d98e24b3
luciatan <lucia.tan@mail.utoronto.ca>
2022-11-19 06:10:02 -0800

    Before, UI needs to take in presenter and presenter needs to take in UI. So I make a setter method in presenter to set the UI instead.

--- src/main/java/mealplan/MealplanOutputBoundary.java
+++ src/main/java/mealplan/MealplanOutputBoundary.java
@@ -9,4 +9,6 @@ public interface MealplanOutputBoundary {
     public void createCaloriesView(List<Integer> cal);
 
     public void addRecipe(String recipe, int meal, RecipeInterActor recipeInteractor);
+
+    public void setUI (MealplanBox mealplanbox);
 }
--- src/main/java/mealplan/MealplanPresenter.java
+++ src/main/java/mealplan/MealplanPresenter.java
@@ -5,10 +5,8 @@ import recipe.RecipeInterActor;
 import java.util.List;
 
 public class MealplanPresenter implements MealplanOutputBoundary{
-    private final MealplanBox UI;
-    public MealplanPresenter(MealplanBox UI){
-        this.UI = UI;
-    }
+    private MealplanBox mealplanBox;
+
     @Override
     public void createCaloriesView(List<Integer> cal) {
         System.out.println("Recommended:" + cal.get(0) + "cal; " +
@@ -17,7 +15,11 @@ public class MealplanPresenter implements MealplanOutputBoundary{
 
     @Override
     public void addRecipe(String recipe, int meal, RecipeInterActor recipeInteractor) {
-        UI.setMealplan(recipe, meal, recipeInteractor);
+        mealplanBox.setMealplan(recipe, meal, recipeInteractor);
+    }
+    @Override
+    public void setUI (MealplanBox mealplanbox) {
+        this.mealplanBox=mealplanbox;
     }
 
 
