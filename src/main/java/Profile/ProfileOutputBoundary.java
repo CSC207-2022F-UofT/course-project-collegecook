e89b3891a075aac18e48bc31b1d84251e1b2db01
yadingfeng <yd.feng@mail.utoronto.ca>
2022-11-19 14:26:14 -0500

    no big change

--- src/main/java/Profile/ProfileOutputBoundary.java
+++
@@ -1,9 +0,0 @@
-package profile;
-
-import entities.Profile;
-
-public interface ProfileOutputBoundary {
-    void view_reviewed(Profile p);
-    void view_created(Profile p);
-    void set_info_success();
-}
