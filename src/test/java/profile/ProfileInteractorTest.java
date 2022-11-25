cea7345e4dfef6ed298ca7b8dedd8db8d2027120
Allison <jinping.liang@mail.utoronto.ca>
2022-11-25 15:31:52 -0500

    Added tests to ProfileRepoImplTest.

--- src/test/java/profile/ProfileInteractorTest.java
+++ src/test/java/profile/ProfileInteractorTest.java
@@ -1,5 +1,29 @@
 package profile;
 
+import entities.Profile;
+import entities.RecipeList;
+import org.junit.jupiter.api.BeforeEach;
+import recipe.RecipeInteractor;
+import recipe.RecipeOutputBoundary;
+import recipe.RecipePresenter;
+import recipe.RecipeRepoGateway;
+import ui.AppController;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class ProfileInteractorTest {
-    // todo
+    ProfileInteractor pi;
+    ProfileRepoGateway prg;
+    ProfileController pc;
+    ArrayList<Profile> temp;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        ProfileOutputBoundary output = new ProfilePresenter();
+        prg = ProfileRepoImpl.getPrl();
+        pi = new ProfileInteractor(output);
+        pc = new ProfileController(pi);
+    }
+
 }
