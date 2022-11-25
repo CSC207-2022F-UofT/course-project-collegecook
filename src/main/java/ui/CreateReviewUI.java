5cd4f7b9d11e46bd2fd33e91cf26e461b981d0f0
kahnj <jason.kahn@mail.utoronto.ca>
2022-11-25 15:57:31 -0500

    Finished CreateReviewUI

--- src/main/java/ui/CreateReviewUI.java
+++ src/main/java/ui/CreateReviewUI.java
@@ -1,5 +1,7 @@
 package ui;
 
+import review.ReviewController;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -9,6 +11,7 @@ import java.util.Arrays;
 public class CreateReviewUI extends JFrame {
 
     AppController appController;
+    ReviewController reviewController;
     JTextField rating = new JTextField(40);
     JTextArea content = new JTextArea(5, 40);
     JLabel ratingQ = new JLabel("Please give your rating of the recipe (from 1-5)");
@@ -19,6 +22,7 @@ public class CreateReviewUI extends JFrame {
 
     public CreateReviewUI(AppController appController){
         this.appController = appController;
+        this.reviewController = appController.getReviewController();
         view.setLayout(new BoxLayout(view, BoxLayout.Y_AXIS));
 
         view.setLayout(new BoxLayout(view, BoxLayout.Y_AXIS));
@@ -32,19 +36,34 @@ public class CreateReviewUI extends JFrame {
         view.add(contentQ);
         content.setAlignmentX(Component.CENTER_ALIGNMENT);
         view.add(content);
-
-        this.add(view);
-        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.pack();
+        String user = appController.getLoginControllor().preformGetLoggedInUser();
+        String recipe = appController.getRecipeController().getReadingRecipe();
 
         save.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                dispose();
 
+                try{
+                    Integer.parseInt(rating.getText());
+                }catch (NumberFormatException e1){
+                    JOptionPane.showMessageDialog(null,
+                            "Please enter an INTEGER for rating!");
+                }
+                int ratenum = Integer.parseInt(rating.getText());
+                if (content.getText().equals("")){
+                    reviewController.performCreateReview(user, recipe, ratenum);
+                } else {
+                    reviewController.performCreateReview(user, recipe, content.getText(), ratenum);
+                }
+
+                dispose();
             }
         });
+        view.add(save);
 
+        this.add(view);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.pack();
 
     }
 }
