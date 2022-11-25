f18401188b4a70e33f394748deca86270ba79440
Allison <jinping.liang@mail.utoronto.ca>
2022-11-25 15:03:45 -0500

    Added test to ProfileRequestModelTest

--- src/test/java/profile/ProfileRequestModelTest.java
+++ src/test/java/profile/ProfileRequestModelTest.java
@@ -1,5 +1,40 @@
 package profile;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ProfileRequestModelTest {
-    // todo
+    ProfileRequestModel prm;
+
+    @BeforeEach
+    void setUp() {
+        prm = new ProfileRequestModel(20, 163, 45, "Female");
+    }
+
+    @AfterEach
+    void tearDown(){
+        prm = null;
+    }
+
+    @Test
+    void testGetAgePrm(){
+        assertEquals(20, prm.getAge());
+    }
+
+    @Test
+    void testGetHeightPrm(){
+        assertEquals(163, prm.getHeight());
+    }
+    @Test
+    void testGetWeightPrm(){
+        assertEquals(45, prm.getWeight());
+    }
+    @Test
+    void testGetGenderPrm(){
+        assertEquals("Female", prm.getGender());
+    }
+
 }
