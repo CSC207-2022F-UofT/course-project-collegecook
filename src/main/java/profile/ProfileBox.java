a8b250b747b7f4627ed17dc47621672ca20db569
Allison <jinping.liang@mail.utoronto.ca>
2022-11-28 14:40:15 -0500

    Added comments in ProfileBox

--- src/main/java/profile/ProfileBox.java
+++ src/main/java/profile/ProfileBox.java
@@ -1,9 +1,26 @@
 package profile;
 
+/**
+ *This is an interface between the view layer and the presenter layer. Related UI should implement this interface.
+ */
 public interface ProfileBox {
-
+    /**
+     * Present the interface of noCreated when the user doesn't have any created recipe.
+     */
     void noCreated();
+
+    /**
+     * Present the interface of hasCreated when the user has created recipes.
+     */
     void hasCreated(String result);
+
+    /**
+     * Present the interface of noReviewed when the user doesn't review any recipe.
+     */
     void noReviewed();
+
+    /**
+     * Present the interface of hasReviewed when the user has reviewed recipes.
+     */
     void hasReviewed(String result);
 }
