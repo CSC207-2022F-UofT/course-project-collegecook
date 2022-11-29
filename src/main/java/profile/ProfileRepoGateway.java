3341762cf688d938420dbd9c72cd91270bfa3bf0
Allison <jinping.liang@mail.utoronto.ca>
2022-11-28 19:31:59 -0500

    Added comments in gateway interface.

--- src/main/java/profile/ProfileRepoGateway.java
+++ src/main/java/profile/ProfileRepoGateway.java
@@ -6,6 +6,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public interface ProfileRepoGateway {
+    /**
+     * This interface is implemented by ProfileRepoImpl to get and save profiles.
+     */
+
     ArrayList<Profile> getProfile() throws IOException;
 
     void saveProfile(ArrayList<Profile> pro) throws IOException;
