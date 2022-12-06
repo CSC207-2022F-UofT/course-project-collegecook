3f807b907ff34018f158148cc5261608a64caed9
krutikajoshi <joshikrutikassc@gmail.com>
2022-12-06 11:20:00 -0500

    updated

--- src/test/java/rank/RankInteractorTest.java
+++ src/test/java/rank/RankInteractorTest.java
@@ -1,39 +1,70 @@
 package rank;
-
-import entities.AverageRatings;
-import entities.Profile;
-import entities.Recipe;
-import entities.User;
 import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
+
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RankInteractorTest {
 
+    RankOutputBoundary rankOutputBoundary = new RankOutputBoundary() {
+        @Override
+        public void prepareSuccessView(RankResponseModel rank) {
+
+        }
+
+        @Override
+        public void prepareFailView(String error) {
+
+        }
+
+        @Override
+        public void setUI(RankResultBox rankResultBox) {
+
+        }
+    };
+    RankInteractor rankInteractor = new RankInteractor(rankOutputBoundary);
+
+
+    public RankInteractorTest() throws IOException {
+    }
 
     @Test
-    public void TestEmptyInteractor() {
-        User[] users = {};
-        String rank = "total followers";
-        Arrays.sort(users, new AvgRatingComparator());
-        int expected = 0;
-        int actual = users.length;
+    public void TestallCreateInteractor() throws IOException {
+        RankOutputBoundary rankOutputBoundary = new RankPresenter();
+        RankInteractor rankInteractor = new RankInteractor(rankOutputBoundary);
+        int actual = rankInteractor.allCreatedRanking("user1");
+        int expected = 5;
         assertEquals(expected, actual);
     }
 
     @Test
-    public void TestInteractorTotal() {
-        User[] users = {};
-        String rank = "total followers";
-        Arrays.sort(users, new AvgRatingComparator());
-        int expected = 0;
-        int actual = users.length;
+    public void TestTotalFollowersInteractor() throws IOException {
+        // Dependent on the saved data file of users, so it might cause errors on different computers
+        String ranking = "total followers";
+        RankRequestModel rankRequestModel = new RankRequestModel(ranking);
+        RankResponseModel returned = rankInteractor.sortUsers(rankRequestModel);
+        int actual = returned.getUsers().size();
+        int expected = 15;
+        String actual1 = returned.getUsers().get(0);
+        String expected1 = "3-Brenden";
         assertEquals(expected, actual);
+        assertEquals(actual1,expected1);
+    }
+
+    @Test
+    public void TestRecipeInteractor() throws IOException {
+        // Dependent on the saved data file of users, so it might cause errors on different computers
+        String ranking = "total number of recipe";
+        RankRequestModel rankRequestModel = new RankRequestModel(ranking);
+        RankResponseModel returned = rankInteractor.sortUsers(rankRequestModel);
+        int actual = returned.getUsers().size();
+        int expected = 15;
+        String actual1 = returned.getUsers().get(0);
+        String expected1 = "5-user1";
+        assertEquals(expected, actual);
+        assertEquals(actual1,expected1);
     }
 
 
