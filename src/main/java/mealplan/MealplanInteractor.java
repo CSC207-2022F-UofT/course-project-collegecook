86f53c8a29440cc50aaaf575fb70986c36d21b37
luciatan <lucia.tan@mail.utoronto.ca>
2022-12-08 05:13:18 -0800

    nothing changed

--- src/main/java/mealplan/MealplanInteractor.java
+++ src/main/java/mealplan/MealplanInteractor.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-
 public class MealplanInteractor implements MealplanInputBoundary{
     private Mealplan mealplan;
     private final String username;
